56f3c98debc19284cf2894feb8d60487e53c007d
YourBoyRory <yourboyrory@gmail.com>
2023-12-23 00:07:07 -0500

    Fixed JRE issue for windows relase

--- LCMM.java
+++ LCMM.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class LCMM {
 
-    public static final String VERSION_STRING = "Lethal Manager v0.2.0";
+    public static final String VERSION_STRING = "Lethal Manager v1.0.1";
 
     public static void main(String args[]) {
         try {
